3752f95f4a64b28b57f396f9efe6c5a15757f291
KaraTyse <karinatyseviciute@gmail.com>
2023-01-13 14:04:53 +0200

    Keitimai su modeliais, nuji pavadinimai, rodymai

--- mysite/autoservisas/admin.py
+++ mysite/autoservisas/admin.py
@@ -1,10 +1,33 @@
 from django.contrib import admin
 from .models import AutoModel, Car, Order, OrderCar, Service
 
+
 # Register your models here.
 
+# padaryti kad uzsakyme galima butu pamatyti ir uzsakomasinu pridejimas
+class OrderCarInline(admin.TabularInline):
+    model = OrderCar
+    can_delete = False
+    extra = 0
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('car', 'date')
+    inlines = [OrderCarInline]  # prijungimas uzsakymomasinos prie uzsakymo
+
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('client', 'valst_nr', 'vin_code')  # Kad butu rodomas ekranas su isvardintais pavadinimais stulepio pavidalu
+    list_filter = ('client', 'automodel')  # filtras
+    search_fields = ('valst_nr', 'vin_code')  # paieska pagal
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+
+
 admin.site.register(AutoModel)
-admin.site.register(Car)
-admin.site.register(Order)
-admin.site.register(Service)
+admin.site.register(Car, CarAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Service, ServiceAdmin)
 admin.site.register(OrderCar)
--- mysite/autoservisas/models.py
+++ mysite/autoservisas/models.py
@@ -2,10 +2,15 @@ from django.db import models
 from django.urls import reverse
 import uuid
 
+
 # Create your models here.
 class AutoModel(models.Model):
     make = models.CharField('Make name', max_length=50, help_text='Enter a car make')
     model = models.CharField('Model name', max_length=50, help_text='Enter a car model')
+
+    class Meta:
+        verbose_name = "Automodel"
+        verbose_name_plural = "Automodels"
 
     def __str__(self):
         return f"{self.make}, {self.model}"
@@ -17,26 +22,39 @@ class Car(models.Model):
     client = models.CharField('Client', max_length=200, help_text='Enter client name and last name')
     automodel = models.ForeignKey('AutoModel', on_delete=models.SET_NULL, null=True)
 
+    # pervadinti laukus klasiu
+    class Meta:
+        verbose_name = "Car"
+        verbose_name_plural = "Cars"
+
     def __str__(self):
         return f"{self.automodel} - {self.client}: {self.valst_nr} - {self.vin_code}"
-
-
 
 
 class Service(models.Model):
     name = models.CharField('Name', max_length=200, help_text='Enter a service name')
     price = models.FloatField('Price', help_text='Enter a service price')
 
+    class Meta:
+        verbose_name = "Service"
+        verbose_name_plural = "Services"
+
     def __str__(self):
         return f"{self.name}: {self.price}"
+
 
 class Order(models.Model):
     date = models.DateTimeField('Date', null=True, blank=True)
     suma = models.FloatField('Sum', help_text='Enter a sum of order')
     car = models.ForeignKey('Car', on_delete=models.SET_NULL, null=True)
 
+    class Meta:
+        verbose_name = "Order"
+        verbose_name_plural = "Orders"
+
     def __str__(self):
-        return f"{self.date}, {self.suma}"
+        return f"{self.car}: {self.date}, {self.suma}"
+
 
 class OrderCar(models.Model):
     kiekis = models.IntegerField('How', help_text='Enter how much do you want to buy car')
@@ -44,6 +62,9 @@ class OrderCar(models.Model):
     order = models.ForeignKey('Order', on_delete=models.SET_NULL, null=True)
     service = models.ForeignKey('Service', on_delete=models.SET_NULL, null=True)
 
+    class Meta:
+        verbose_name = "Ordercar"
+        verbose_name_plural = "Ordercars"
+
     def __str__(self):
         return f"{self.order}, {self.service.name} - {self.kiekis}: {self.kaina}"
-

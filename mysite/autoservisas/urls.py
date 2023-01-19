524ee5bd7a44ff5fa1a33afa308863d7eccfda4f
KaraTyse <karinatyseviciute@gmail.com>
2023-01-19 12:15:16 +0200

    added search by auto models, vehicle registration number, vin code and owner

--- mysite/autoservisas/urls.py
+++ mysite/autoservisas/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('car/', views.cars, name='car'),
     path('car/<int:car_id>', views.car, name='car-detail'),
     path('orders/', views.OrderListView.as_view(), name='orders'),
-    path('orders/<int:pk>', views.OrderDetailView.as_view(), name='orer-detail')
+    path('orders/<int:pk>', views.OrderDetailView.as_view(), name='orer-detail'),
+    path('search/', views.search, name='search'),
 ]
--- mysite/autoservisas/views.py
+++ mysite/autoservisas/views.py
@@ -2,14 +2,12 @@ from .models import Car, AutoModel, OrderCar, Order, Service
 from django.views import generic
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
-
-
+from django.db.models import Q
 
 from django.http import HttpResponse
 
 
 def index(request):
-
     num_service = Service.objects.all().count()
 
     num_order = Order.objects.all().count()
@@ -23,6 +21,7 @@ def index(request):
                'num_car': num_car,
                'num_automodel': num_automodel}
     return render(request, 'index.html', context=context)
+
 
 def cars(request):
     # cars = Car.objects.all()
@@ -43,7 +42,17 @@ class OrderListView(generic.ListView):
     paginate_by = 1
     template_name = 'order_list.html'
 
+
 class OrderDetailView(generic.DetailView):
     model = Order
     template_name = 'order_detail.html'
 
+
+def search(request):
+    query = request.GET.get("query")
+    search_results = Car.objects.filter(
+        Q(client__icontains=query) | Q(valst_nr__icontains=query) |
+        Q(vin_code__icontains=query)
+    )
+    return render(request, 'search.html', {"cars": search_results,
+                                           "query": query})
